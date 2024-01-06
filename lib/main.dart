f0f3bb5cbd61d2348775749c3aed2770bbe74918
kenan7Th <thbeankenan@gmail.com>
2024-01-06 10:43:20 -0800

    version 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,70 @@ class PointsCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('Points Counter'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              'Team A ',
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            Text(
+              '0 ',
+              style: TextStyle(
+                fontSize: 150,
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                minimumSize: Size(150, 50),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Add 1 Point',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                minimumSize: Size(150, 50),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Add 1 Point',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                minimumSize: Size(150, 50),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Add 3 Point',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
